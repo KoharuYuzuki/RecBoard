b3cb4722a948676e18a27cbff09d1052f3f596b4
KoharuYuzuki <79073521+KoharuYuzuki@users.noreply.github.com>
2023-10-02 20:34:56 +0900

    zipファイル名にバージョンを記載するように変更

--- zip.sh
+++ zip.sh
@@ -1,6 +1,8 @@
+VERSION=$(node -p "require('./package.json').version")
+
 pushd 'dist'
 
-ditto -c -k "./mac/"       "RecBoard-mac-x64.zip"
-ditto -c -k "./mac-arm64/" "RecBoard-mac-arm64.zip"
+ditto -c -k "./mac/"       "RecBoard-v${VERSION}-mac-x64.zip"
+ditto -c -k "./mac-arm64/" "RecBoard-v${VERSION}-mac-arm64.zip"
 
 popd
